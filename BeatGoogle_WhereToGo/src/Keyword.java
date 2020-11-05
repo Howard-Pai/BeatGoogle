e174368cd294855887547122a78d5c5520c032ac
Noxren <noxren0821@gmail.com>
2020-11-05 22:53:05 +0800

    test

---
+++ BeatGoogle_WhereToGo/src/Keyword.java
@@ -0,0 +1,13 @@
+
+public class Keyword {
+	
+	private String name;
+	private int count;
+	private float weight;
+	
+	public Keyword(String name, int count, float weight) {
+		this.name = name;
+		this.count = count;
+		this.weight = weight;
+	}
+}
---
+++ BeatGoogle_WhereToGo/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("欸你這週要去哪");
+	}
+
+}
